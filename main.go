bcc248339617d3f5848abed1c8de8058fa4fd57b
PapaEmeritus4 <vovakaiman2005@gmail.com>
2024-07-24 14:40:55 +0300

    Finished conditional and logical operators tutorial

--- main.go
+++ main.go
@@ -3,85 +3,49 @@ package main
 import "fmt"
 
 func main() {
-	x, y := 5, 3
-	xf, yf := 3.0, 2.0
+	var firstVariable bool
+	fmt.Println(firstVariable)
 
-	greet()
-	greetWithParameter("Vova")
-	greetWithParameters("Vova", "Bilan")
-	fmt.Println("Result of sum: ", sum(x, y))
-	sum, mult := sumAndMultiply(x, y)
-	fmt.Printf("Result of sum and multiply: %d and %d\n", sum, mult)
-	_, mult32 := namedSumAndMultiply(x, y) // var _ - proxy
-	fmt.Printf("Result of multiply: %d\n", mult32)
+	secondVariable := true
+	fmt.Println(secondVariable)
 
-	/*  func as var  */
-	var multiplier func(x, y int) int
-	multiplier = func(x, y int) int { return x * y }
-	fmt.Printf("Result of func multiply as var: %d\n", multiplier(x, y))
+	age := 15
 
-	divider := func(x, y float32) float32 { return float32(x / y) }
-	fmt.Printf("Result of func divide as var: %f\n", divider(float32(xf), float32(yf)))
-	/*  func as var  */
+	// Basic if
+	if age < 18 { // or: if (age < 18) == true {
+		fmt.Println("You are too young (full)")
+	}
 
-	// func like parameter
-	sumFunc := func(x, y int) int { return x + y }
-	subFunc := func(x, y int) int { return x - y }
-	fmt.Printf("Result of calculate: %d\n", calculate(x, y, sumFunc))
-	fmt.Printf("Result of calculate: %d\n", calculate(x, y, subFunc))
+	//Short syntax
+	if isChild := isChildren(age); isChild == true { // shorter: if isChild := isChildren(age); isChild {
+		fmt.Println("You are too young (short)")
+		fmt.Println(isChild)
+	}
 
-	// Return new function
-	divideBy2 := createDivider(2)
-	divideBy10 := createDivider(10)
-	fmt.Println(divideBy2(100))
-	fmt.Println(divideBy10(100))
+	// If ... else
+	age = 20
+	if age < 18 {
+		fmt.Println("you are too young")
+	} else {
+		fmt.Println("You are an adult")
+	}
 
-	//getter
-	usd := 30
-	getUsdValue := func() int { return usd }
-	fmt.Println(getUsdValue())
-}
+	// &&
+	if age >= 7 && age <= 18 {
+		fmt.Println("You are in school")
+	}
 
-func greet() {
-	fmt.Println("Wusup")
-}
+	// ||
+	if age == 14 || age == 20 || age == 40 {
+		fmt.Println("You have to get a new passport")
+	}
 
-func greetWithParameter(name string) {
-	fmt.Printf("Hello, %s!\n", name)
-}
-
-func greetWithParameters(name, surname string) {
-	fmt.Printf("Hello, %s %s!\n", name, surname)
-}
-
-func sum(first int, second int) int {
-	return first + second
-}
-
-func sumAndMultiply(first int, second int) (int, int) {
-	return first + second, first * second
-}
-
-func namedSumAndMultiply(first int, second int) (sum int32, mult int32) {
-	sum = int32(first + second)
-	mult = int32(first * second)
-	return // or return sum, mult
-}
-
-// func like parameter
-func calculate(x, y int, action func(x, y int) int) int {
-	return action(x, y)
-}
-
-// in func we return new func
-func createDivider(divider int) func(x int) int {
-	return func(x int) int {
-		return x / divider
+	// !
+	if !isChildren(age) {
+		fmt.Println("You are an adult")
 	}
 }
 
-/*
-	function naming
-	main() = private
-	Main() = public
-*/
+func isChildren(age int) bool {
+	return age < 18
+}
